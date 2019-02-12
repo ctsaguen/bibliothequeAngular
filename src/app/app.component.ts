6ff9badece0787de9170be2671f7ad41621a7c6b
ctsaguen <kevintsague1@gmail.com>
2019-02-12 20:36:52 +0100

    un seul commit pour sauvegarder mon projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Bibliotheque';
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyAz8qlISRLROttTk-lsNN0nhZU49Ev5y9o",
+      authDomain: "tpopenclassroom-67d99.firebaseapp.com",
+      databaseURL: "https://tpopenclassroom-67d99.firebaseio.com",
+      projectId: "tpopenclassroom-67d99",
+      storageBucket: "tpopenclassroom-67d99.appspot.com",
+      messagingSenderId: "61779841303"
+    };
+    firebase.initializeApp(config);
+
+  }
 }
